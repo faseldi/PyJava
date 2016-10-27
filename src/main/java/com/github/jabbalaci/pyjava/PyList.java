dd9e3e55fb06d87d9faf28e2b5dd5b3cd607228b
Faseldi <florianduneaufamily@hotmail.fr>
2016-10-28 01:31:04 +0200

    PyList now implements IPyLst and extends ArrayList to not to have to implement non needed methods slice(begin, end)  is implemented and tested

--- src/main/java/com/github/jabbalaci/pyjava/PyList.java
+++ src/main/java/com/github/jabbalaci/pyjava/PyList.java
@@ -1,19 +1,60 @@
 package com.github.jabbalaci.pyjava;
 
+import java.util.ArrayList;
 import java.util.Collections;
-
-/**
- * List methods.
- * List helpers.
- *
- * @author jabba
- */
-
 import java.util.List;
+import java.util.stream.IntStream;
 
 
-public class PyList {
-
+public class PyList<T> extends ArrayList<T> implements IPyList<T>{
+    
+    public PyList(){
+        super();
+    }
+    
+    public PyList(int size){
+        super(size);
+    }
+    /**
+     * This method works like [begin:end] python do
+     * @param begin The begining index
+     * @param end The starting index
+     * @return A new IPyList sliced
+     */
+    @Override
+    public IPyList<T> slice(int begin, int end) {
+        int size = size();
+        // adapt begin and end to work like python
+        if(begin < 0){
+            begin = size + begin;
+        }else{
+            // if begin > 0 && end > 0
+            if(end > 0){
+                begin ++;
+                end ++;
+            }
+        }
+        if(end < 0){
+            end = size+ end;
+        }
+        
+        PyList<T> slice = new PyList<>();
+        // to avoid out of bounds and useless iterate
+        if(begin < 0 ) {
+            begin = 0;
+        }
+        if(end < 0 ) {
+            end = 0;
+        }
+        // check if need to iterate
+        if(begin != end){
+            IntStream.range(begin, end)
+                    .forEach( i -> {
+                        slice.add(this.get(i));
+                    });
+        }
+        return slice;
+    }
     /**
      * Sort the list in place. It has no return value.
      *
